099c91dc7b2c4e6c9eeffe65e05308a5d951a496
Alex Dunn <alexander.dunn@outlook.com>
2019-06-12 09:28:51 -0400

    scaffolding a basic abstraction sample

---
+++ Samples/Abstracting/AbstractingSamples/AbstractingSamples/Controllers/SimpleAlexaController.cs
@@ -0,0 +1,20 @@
+﻿using Alexa.NET.Request;
+using Alexa.NET.Response;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbstractingSamples.Controllers
+{
+    [Route("[controller]")]
+    public class SimpleAlexaController : Controller
+    {
+        [HttpPost]
+        public async Task<SkillResponse> HandleRequest([FromBody]SkillRequest request)
+        {
+            return null;
+        }
+    }
+}
