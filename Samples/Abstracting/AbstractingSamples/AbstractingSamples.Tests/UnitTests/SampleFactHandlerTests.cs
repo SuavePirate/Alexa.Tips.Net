4935d51acb6982fef774bb33b2f549ef7cd1907d
Alex Dunn <alexander.dunn@outlook.com>
2019-06-25 18:19:02 -0400

    scaffolding tests

---
+++ Samples/Abstracting/AbstractingSamples/AbstractingSamples.Tests/UnitTests/SampleFactHandlerTests.cs
@@ -0,0 +1,23 @@
+﻿using AbstractingSamples.Handlers.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AbstractingSamples.Tests.UnitTests
+{
+    public class SampleFactHandlerTests
+    {
+        [Fact]
+        public async Task SampleFactHandler_ReturnResponseWithData()
+        {
+            // TODO: create subject with moq of db context
+        }
+        [Fact]
+        public async Task SampleFactHandler_ReturnFallbackResponseWithNoData()
+        {
+            // TODO: create subject with moq of db context
+        }
+    }
+}
