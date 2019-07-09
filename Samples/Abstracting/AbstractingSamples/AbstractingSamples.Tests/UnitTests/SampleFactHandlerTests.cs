1715aaf9615d4e050781e635145eaaca4ff1043d
Alex Dunn <alexander.dunn@outlook.com>
2019-07-08 20:12:16 -0400

    updated some tests

--- Samples/Abstracting/AbstractingSamples/AbstractingSamples.Tests/UnitTests/SampleFactHandlerTests.cs
+++ Samples/Abstracting/AbstractingSamples/AbstractingSamples.Tests/UnitTests/SampleFactHandlerTests.cs
@@ -1,7 +1,15 @@
-﻿using AbstractingSamples.Handlers.Implementation;
+﻿using AbstractingSamples.Contexts;
+using AbstractingSamples.Handlers.Implementation;
+using AbstractingSamples.Models;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,12 +20,62 @@ namespace AbstractingSamples.Tests.UnitTests
         [Fact]
         public async Task SampleFactHandler_ReturnResponseWithData()
         {
-            // TODO: create subject with moq of db context
+            // arrange
+            var context = new Mock<SampleMessageDbContext>();
+            context.Setup(d => d.SampleMessages.FirstOrDefaultAsync(CancellationToken.None)).Returns(Task.FromResult(new SampleMessage
+            {
+                Id = Guid.NewGuid().ToString(),
+                Content = "This is a mocked response message"
+            }));
+            var subject = new SampleFactHandler(context.Object);
+
+            // arrange
+            var request = new SkillRequest()
+            {
+                Version = "1.0",
+                Request = new IntentRequest
+                {
+                    Type = "IntentRequest",
+                    Intent = new Intent
+                    {
+                        Name = "SampleMessageIntent"
+                    }
+                }
+            };
+
+            // act
+            var response = await subject.HandleAsync(request);
+
+            // assert
+            Assert.NotNull((response.Response.OutputSpeech as PlainTextOutputSpeech)?.Text);
         }
         [Fact]
         public async Task SampleFactHandler_ReturnFallbackResponseWithNoData()
         {
-            // TODO: create subject with moq of db context
+            // arrange
+            var context = new Mock<SampleMessageDbContext>();
+            context.Setup(d => d.SampleMessages.FirstOrDefaultAsync(CancellationToken.None)).Returns(Task.FromResult<SampleMessage>(null));
+            var subject = new SampleFactHandler(context.Object);
+
+            // arrange
+            var request = new SkillRequest()
+            {
+                Version = "1.0",
+                Request = new IntentRequest
+                {
+                    Type = "IntentRequest",
+                    Intent = new Intent
+                    {
+                        Name = "SampleMessageIntent"
+                    }
+                }
+            };
+
+            // act
+            var response = await subject.HandleAsync(request);
+
+            // assert
+            Assert.True((response.Response.OutputSpeech as PlainTextOutputSpeech)?.Text == "I don't have any messages for you.");
         }
     }
 }
